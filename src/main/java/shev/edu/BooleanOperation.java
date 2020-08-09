ba38d89a3880286861607b7c63d0d7acbad7519a
flax87 <Gendalf2332@gmail.com>
2020-08-09 19:36:52 +0900

    Initial commit

---
+++ src/main/java/shev/edu/BooleanOperation.java
@@ -0,0 +1,51 @@
+/*
+ * Class name :  PalindromeService
+ *
+ * @author Oleksii Shevchenko
+ *
+ * Description:
+ *
+ * 1) Implement static method isValid(...){...}
+ * 2) Logic of method isValid(...){...} has to be encapsulated
+ * 3) Implement logger and test for each method of class PalindromeService
+ * 4) Implement cyrillic symbols check
+ *
+ */
+
+package shev.edu;
+
+import java.util.logging.Logger;
+
+public class BooleanOperation{
+
+    //params
+    private Boolean firstOne;
+    private Boolean secondOne;
+
+    //Logger to log
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    //constructor
+    public BooleanOperation(Boolean firstOne, Boolean secondOne){
+        this.firstOne = firstOne;
+        this.secondOne = secondOne;
+
+
+    }
+    //Methods conjunction operation
+    public Boolean conjunction(){
+        LOGGER.info("Started conjunction operation");
+        if (this.firstOne == null || this.secondOne == null) return null;
+        return this.firstOne && this.secondOne;
+    }
+
+    public Boolean disjunction() {
+        LOGGER.info("Started disjunction operation");
+        if (this.firstOne == null && this.secondOne == null) return null;
+        if (this.firstOne == null) return secondOne;
+        if (this.secondOne == null) return firstOne;
+        return this.firstOne || this.secondOne;
+    }
+
+
+}
---
+++ src/test/java/shev/edu/BooleanOperationTest.java
@@ -0,0 +1,137 @@
+
+/*
+ * Class name :  PalindromeService
+ *
+ * @author Oleksii Shevchenko
+ *
+ * Description:
+ *
+ * 1) Implement static method isValid(...){...}
+ * 2) Logic of method isValid(...){...} has to be encapsulated
+ * 3) Implement logger and test for each method of class PalindromeService
+ * 4) Implement cyrillic symbols check
+ *
+ */
+package shev.edu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BooleanOperationTest {
+
+    Boolean result;
+
+//Tests
+    @Test
+    public void whenFirstTrueSecondTrueConjunctionTrue(){
+        assertEquals(true, result = new BooleanOperation(true, true).conjunction());
+    }
+
+
+    @org.testng.annotations.Test
+    public void whenFirstTrueSecondFalseConjunctionFalse() {
+        assertEquals(false, result = new BooleanOperation(true, false).conjunction()
+        );
+    }
+
+    @Test
+    public void whenFirstTrueSecondNullConjunctionNull() {
+        assertEquals(null, result = new BooleanOperation(true, null).conjunction()
+        );
+    }
+
+
+    @Test
+    public void whenFirstFalseSecondTrueConjunctionFalse() {
+        assertEquals(false, result = new BooleanOperation(false, true).conjunction()
+        );
+    }
+
+    @Test
+    public void whenFirstFalseSecondFalseConjunctionFalse() {
+        assertEquals(false, result = new BooleanOperation(false, false).conjunction()
+        );
+    }
+
+    @Test
+    public void whenFirstFalseSecondNullConjunctionNull() {
+        assertEquals(null, result = new BooleanOperation(false, null).conjunction()
+        );
+    }
+
+    @Test
+    public void whenFirstNullSecondTrueConjunctionNull() {
+        assertEquals(null, result = new BooleanOperation(null, true).conjunction()
+        );
+    }
+
+    @Test
+    public void whenFirstNullSecondFalseConjunctionNull() {
+        assertEquals(null, result = new BooleanOperation(null, false).conjunction()
+        );
+    }
+
+    @Test
+    public void whenFirstNullSecondNullConjunctionNull() {
+        assertEquals(null, result = new BooleanOperation(null, null).conjunction()
+        );
+    }
+
+
+    @Test
+    public void whenFirstTrueSecondTrueDisjunctionTrue() {
+        assertEquals(true, result = new BooleanOperation(true, true).disjunction()
+        );
+    }
+
+    @Test
+    public void whenFirstTrueSecondFalseDisjunctionTrue() {
+        assertEquals(true, result = new BooleanOperation(true, false).disjunction()
+        );
+    }
+
+    @Test
+    public void whenFirstTrueSecondNullDisjunctionTrue() {
+        assertEquals(true, result = new BooleanOperation(true, null).disjunction()
+        );
+    }
+
+
+    @Test
+    public void whenFirstFalseSecondTrueDisjunctionTrue() {
+        assertEquals(true, result = new BooleanOperation(false, true).disjunction()
+        );
+    }
+
+    @Test
+    public void whenFirstFalseSecondFalseDisjunctionFalse() {
+        assertEquals(false, result = new BooleanOperation(false, false).disjunction()
+        );
+    }
+
+    @Test
+    public void whenFirstFalseSecondNullDisjunctionFalse() {
+        assertEquals(false, result = new BooleanOperation(false, null).disjunction()
+        );
+    }
+
+
+    @Test
+    public void whenFirstNullSecondTrueDisjunctionTrue() {
+        assertEquals(true, result = new BooleanOperation(null, true).disjunction()
+        );
+    }
+
+    @Test
+    public void whenFirstNullSecondFalseDisjunctionFalse() {
+        assertEquals(false, result = new BooleanOperation(null, false).disjunction()
+        );
+    }
+
+    @Test
+    public void whenFirstNullSecondNullDisjunctionNull() {
+        assertEquals(null, result = new BooleanOperation(null, null).disjunction()
+        );
+    }
+}
